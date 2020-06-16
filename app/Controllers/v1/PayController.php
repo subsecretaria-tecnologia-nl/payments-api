805833924a31d8afafacc3e7e5ea53427efbfc63
Jose Antonio Perez Cordova <joseaperezcordova@gmail.com>
2020-06-15 20:14:14 -0500

    agregar pay controller y el schema del json

---
+++ app/Controllers/v1/PayController.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Controllers\v1;
+use App\Exceptions\ShowableException;
+use App\Utils\Utils;
+
+class PayController {
+    public static function post_index(){
+        
+        return $jsonvalidate;
+    }
+}
