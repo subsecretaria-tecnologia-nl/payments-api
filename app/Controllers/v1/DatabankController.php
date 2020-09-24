9e5b31d7a2c8bebd8afd33e8d697a5f342406709
Jose Antonio Perez Cordova <joseaperezcordova@gmail.com>
2020-09-24 13:27:12 -0500

    Se modifico la forma de obtener los datos del json para las vriable urlretorno y urlconfirmapago

--- app/Controllers/v1/DatabankController.php
+++ app/Controllers/v1/DatabankController.php
@@ -9,10 +9,10 @@ use App\Utils\Utils;
 class DatabankController {
 
     public static function post_index($request) {
-//        DB::listen(function($query) {
-//            //Imprimimos la consulta ejecutada
-//            echo "<pre> {$query->sql } </pre>";
-//        });
+        DB::listen(function($query) {
+            //Imprimimos la consulta ejecutada
+            echo "<pre> {$query->sql } </pre>";
+        });
         extract(get_object_vars($request));
         //obtenemos el metodo y el banco segun la cuenta que seleccionan
         $datosCuenta = DB::table('oper_cuentasbanco as CB')
@@ -25,8 +25,8 @@ class DatabankController {
         $datosTransaccion = DB::table('oper_transacciones as T')
                 ->leftJoin('oper_tramites as Tr', 'T.id_transaccion_motor', '=', 'Tr.id_transaccion_motor')
                 ->select('T.id_transaccion_motor', 'T.referencia', 'T.importe_transaccion', 'Tr.nombre', 'Tr.apellido_paterno', 'Tr.apellido_materno',
-                        'Tr.razon_social', 'Tr.id_tipo_servicio', \DB::raw('JSON_EXTRACT(T.json, "$.url_retorno") url_retorno')
-                        , \DB::raw('JSON_EXTRACT(T.json, "$.url_confirma_pago") url_confirmapago'),
+                        'Tr.razon_social', 'Tr.id_tipo_servicio', \DB::raw('JSON_EXTRACT(CONVERT(T.json,CHAR), "$.url_retorno") url_retorno'),
+                         \DB::raw('JSON_EXTRACT(CONVERT(T.json,CHAR),"$.url_confirma_pago") url_confirmapago'),
                         'T.id_transaccion', 'Tr.id_tramite_motor', 'Tr.id_tramite', 'Tr.importe_tramite')
                 ->where('T.id_transaccion_motor', '=', $folio)
                 ->get();
