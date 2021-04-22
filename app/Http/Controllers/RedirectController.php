476e465d1816156c76524f7fedf1664b54c9da64
Raymundo Salazar <raymundo.salser@hotmail.com>
2021-04-22 09:46:57 -0500

    dev -> master (#39)
    * Variables entorno recibo formato (#31)
    
    * Se agrego una excepcion cuando netpay no responde correctamente
    
    * Se agrego una excepcion cuando netpay no responde correctamente
    
    * Se agrego una excepcion cuando netpay no responde correctamente autor
    
    * Los items en cero no pueden viajar a netpay, se eliminaron de la lista si existieran
    
    * Se agregar a las variables de entorno los recibos y los formatos de pago
    
    Co-authored-by: PECA820312QS5 <PECA820312QS5@DESKTOP-IQUPH2D.genl.nl.gob>
    Co-authored-by: jose antonio perez cordova <>
    
    * remove handler show message on production
    
    * se corrigio la suma de los detalles (#34)
    
    Co-authored-by: José <José@10.164.0.141>
    
    * build to qa and prod
    
    * Create front and redirect controllers and create redirect_with function to send POST or GET data to url_retorno
    
    * se agrego la funcionalidad de retornarcon la respuesta del banco (#36)
    
    Co-authored-by: José <José@10.164.0.141>
    
    * workflows review app
    
    * remove npm
    
    * change deploy name
    
    * add prefix var to change
    
    * add prefix var to change
    
    * Fecha vigencia (#38)
    
    * Se agrego una excepcion cuando netpay no responde correctamente
    
    * Se agrego una excepcion cuando netpay no responde correctamente
    
    * Se agrego una excepcion cuando netpay no responde correctamente autor
    
    * Los items en cero no pueden viajar a netpay, se eliminaron de la lista si existieran
    
    * Se agregar a las variables de entorno los recibos y los formatos de pago
    
    * se agrego funcionalidad de redicreccionamiento con la url especificada en url_retorno
    
    * se agrego el default de la fecha limite de referencia a 3 dias habiles
    
    Co-authored-by: PECA820312QS5 <PECA820312QS5@DESKTOP-IQUPH2D.genl.nl.gob>
    Co-authored-by: jose antonio perez cordova <>
    Co-authored-by: Raymundo Salazar <raymundo.salser@hotmail.com>
    
    Co-authored-by: joseaperezcordova <joseaperezcordova@gmail.com>
    Co-authored-by: PECA820312QS5 <PECA820312QS5@DESKTOP-IQUPH2D.genl.nl.gob>
    Co-authored-by: José <José@10.164.0.141>

--- app/Http/Controllers/RedirectController.php
+++ app/Http/Controllers/RedirectController.php
@@ -8,31 +8,29 @@ use App\Schemas\SchemaValidator;
 use App\Utils\Utils;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use App\Controllers\v1\RespuestabancoController;
 
 class RedirectController extends Controller {
 
-	/**
-	 * Create a new controller instance.
-	 *
-	 * @return void
-	 */
-	public function __construct() {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        
+    }
 
-	}
+    public function index(Request $request) {
+        // CACHAS DEL BANCO
+        // PROCESAS LA INFO
+        // ACTUALIZAS DB
+        // REDIRECCIONAS A LA RUTA DE RETORNO POR POST
+        $b = new RespuestabancoController();
+        $res = $b->post_index($request);
+        $urlRetorno = isset($res['url_response'])?$res['url_response']:"paginaError";
+        $data = ["json"=>json_encode($res['datos'])];
+        return Utils::redirect_with($urlRetorno, $data, 'post');
+    }
 
-	public function index(Request $request){
-		// CACHAS DEL BANCO
-		// PROCESAS LA INFO
-		// ACTUALIZAS DB
-		// REDIRECCIONAS A LA RUTA DE RETORNO POR POST
-
-		$url = "http://10.153.144.218/tramites-ciudadano/cart";
-		$headers = null;
-		$data = [
-			"token" => "LJASHFLJASHFLJAHF",
-			"metodo" => "netpay"
-		];
-		
-		return Utils::redirect_with($url, $data, 'get');
-	}
 }
